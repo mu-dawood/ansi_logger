c793dd47e2b762ab9a5ed39e5dc080e2ff552eea
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-12-07 18:37:33 +0200

    ....

--- README.md
+++ README.md
@@ -19,3 +19,32 @@ see the example
     <td><img src="https://raw.githubusercontent.com/mo-ah-dawood/ansi_logger/main/screen_shots/white_3.png"></td>
   </tr>
 </table>
+
+# Working with dio
+- first create `Interceptor ` and override `onRequest` and `onError` method 
+
+```dart
+
+class _Interceptor extends Interceptor {
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    super.onError(err, handler);
+     // See the example to know how to log this Error
+  }
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    super.onResponse(response, handler);
+     // See the example to know how to log this response
+  }
+}
+
+```
+
+- then you should use this Interceptor
+
+```dart
+ Dio dio = Dio();
+ dio.interceptors.add(_Interceptor());
+ dio.get("https://jsonplaceholder.typicode.com/todos/1");
+ 
+```
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:ansi_logger/ansi_logger.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -130,9 +135,117 @@ class MyHomePage extends StatelessWidget {
                 );
               },
             ),
+            ElevatedButton(
+              child: Text("Dio"),
+              onPressed: () {
+                Dio dio = Dio();
+                dio.interceptors.add(_Interceptor());
+
+                dio.get("https://jsonplaceholder.typicode.com/todos/1");
+              },
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _Interceptor extends Interceptor {
+  final logger = AnsiLogger();
+  DateTime? _start;
+  String? get _duration {
+    if (_start == null) return 'without duration ➤ ';
+    var duration = DateTime.now().difference(_start!).inMilliseconds;
+    return '$duration MS ➤ ';
+  }
+
+  dynamic tryGetMap(dynamic data) {
+    try {
+      if (data is String) {
+        return jsonDecode(data);
+      } else {
+        return data;
+      }
+    } catch (e) {
+      return data;
+    }
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    super.onError(err, handler);
+    if (!kDebugMode) return;
+    logger.logBox('Got error ${err.requestOptions.uri} at ${DateTime.now()}');
+    dynamic requestData;
+    List<IFile>? files;
+    if (err.requestOptions.data is FormData) {
+      var fd = err.requestOptions.data as FormData;
+      requestData = {};
+      for (var element in fd.fields) {
+        requestData[element.key] = element.value;
+      }
+      files = [];
+      for (var k in fd.files) {
+        files.add(IFile(k.value.filename ?? '', k.key, k.value.length));
+      }
+    } else {
+      requestData = err.requestOptions.data;
+    }
+
+    logger.logHttp(
+      url: err.requestOptions.uri.toString(),
+      statusMessage: '$_duration${err.response?.statusMessage}',
+      method: err.requestOptions.method,
+      statusCode: err.response?.statusCode ?? 0,
+      queryParameters: err.requestOptions.queryParameters,
+      requestHeaders: err.requestOptions.headers,
+      responseHeaders: err.response?.headers.map ?? {},
+      response: tryGetMap(err.response?.data),
+      requestData: requestData,
+      files: files,
+    );
+  }
+
+  static String _formatBytes(int bytes, int decimals) {
+    if (bytes <= 0) return '0 B';
+    const suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    var i = (log(bytes) / log(1024)).floor();
+    return ((bytes / pow(1024, i)).toStringAsFixed(decimals)) + ' ' + suffixes[i];
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    super.onResponse(response, handler);
+    if (!kDebugMode) return;
+    logger.logBox('Got response ${response.requestOptions.uri} at ${DateTime.now()}');
+    dynamic requestData;
+    List<IFile>? files;
+    if (response.requestOptions.data is FormData) {
+      var fd = response.requestOptions.data as FormData;
+      requestData = {};
+      for (var element in fd.fields) {
+        requestData[element.key] = element.value;
+      }
+      files = [];
+      for (var k in fd.files) {
+        files.add(IFile(k.value.filename ?? '', k.key, k.value.length));
+      }
+    } else {
+      requestData = response.requestOptions.data;
+    }
+    var bytes = _formatBytes(utf8.encode('${response.data}').length, 2);
+    logger.logHttp(
+      url: response.requestOptions.uri.toString(),
+      statusMessage: '$bytes ➤ $_duration${response.statusMessage}',
+      method: response.requestOptions.method,
+      statusCode: response.statusCode ?? 0,
+      queryParameters: response.requestOptions.queryParameters,
+      requestHeaders: response.requestOptions.headers,
+      responseHeaders: response.headers.map,
+      response: tryGetMap(response.data),
+      requestData: requestData,
+      files: files,
     );
   }
 }
